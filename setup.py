863f201be2703340911ef1b52804a0b1970c7812
shabelski89 <a.shabelsky@gmail.com>
2022-02-01 15:08:46 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pdfhelper',
+    version='1.0.0',
+    packages=['wxgui'],
+    url='https://github.com/shabelski89/pdfhelper',
+    license='',
+    author='Aleksandr Shabelsky',
+    author_email='a.shabelsky@gmail.com',
+    description='GUI to convert docx2pdf and pdf2png',
+    install_requires=['docx2pdf', 'pdf2image', 'wxpython', 'objectlistview'],
+    python_requires='>=3.6'
+)
